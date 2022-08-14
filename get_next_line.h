291b0c663679f2ac0a461d291dcfc1cb8930ac3b
Alexis Abda <aabda@student.s19.be>
2022-08-14 11:46:50 +0200

    final commit

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,29 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 11:17:45 by aabda             #+#    #+#             */
-/*   Updated: 2022/08/08 11:17:47 by aabda            ###   ########.fr       */
+/*   Updated: 2022/08/14 11:41:09 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include <limits.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_find_nl(char *s);
+char	*get_next_line(int fd);
+char	*ft_line(int fd, char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
